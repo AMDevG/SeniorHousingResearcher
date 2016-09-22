0075df90652bcfa266469c8ed63fd56695497e9d
John Berry <John Berry>
2016-09-22 14:32:05 -0500

    Successfully returning Facility List

--- findComps/views.py
+++ findComps/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-
 from .zipcodeSearch import getNearbyZips
-
+from .models import Providerinfo
 
 def find(request):
 	return render(request, 'comps/findSNFs.html' )
@@ -9,12 +8,52 @@ def find(request):
 
 def compsearch(request):
 
+	facility_information = {}
+	comp_objects =[]
+	flat_objects = []
+
 	if request.method == 'POST':
 		zipcode = request.POST.get('zip', None)
 		print("zipcode received ", zipcode)
 		radius = request.POST.get('radius', None)
 		zips = getNearbyZips(zipcode, radius)
 
-		print(zips)
+		for zipcode in zips:
+			addition = Providerinfo.objects.all().filter(zip=zipcode)	# returns QuerySet Object
+			comp_objects.append(addition)
+		print("Length of comp_objects is ", len(comp_objects))
+		
+
+	counter = 0
+	while counter != len(comp_objects):				##FLATTENS INTO STRAIGHT LIST
+		item = comp_objects[counter]
+		for i in item:
+			flat_objects.append(i)
+		counter+=1
+
+	
+	for item in flat_objects:
+		facility_information[item.provname] = {}
+		clean_provname = item.provname
+		clean_comp_address = item.address
+		clean_comp_city = item.city
+		clean_comp_state = item.state
+		clean_comp_zip = item.zip
+		clean_comp_phone = item.phone
+		clean_comp_bedcert = item.bedcert
+		clean_comp_ownertype = item.ownership
+		clean_comp_ownername = item.owner_name
+
+		facility_information[item.provname]['address'] = clean_comp_address
+		facility_information[item.provname]['city'] = clean_comp_city
+		facility_information[item.provname]['state'] = clean_comp_state
+		facility_information[item.provname]['zip'] = clean_comp_zip
+		facility_information[item.provname]['phone'] = clean_comp_phone
+		facility_information[item.provname]['bedcert'] = clean_comp_bedcert
+		facility_information[item.provname]['ownertype'] = clean_comp_ownertype
+		#facility_information[item.provname]['ownername'] = clean_comp_ownername   ####NEED TO MIGRATE OVER FROM OTHER TABLE 
+
+		
+	print(facility_information)
 
 	return render(request, 'comps/findSNFs.html' )
--- findComps/zipcodeSearch.py
+++ findComps/zipcodeSearch.py
@@ -1,10 +1,8 @@
 import pyzipcode
-from .models import Providerinfo
+
 
 def getNearbyZips(targetZip, searchRad):
-    print("Zip being queried", targetZip)
     clean_list = []
     zipcodeDB = pyzipcode.ZipCodeDatabase()
     zipResults = [z.zip for z in zipcodeDB.get_zipcodes_around_radius(targetZip, searchRad)]
-
     return zipResults
